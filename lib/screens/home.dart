44845a43a281a4637970e0be2eb940a622c519e1
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-10-08 16:40:16 -0600

    adding title and description in home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,7 +12,34 @@ class Home extends StatelessWidget {
             children: [
               Image(
                 image: AssetImage('assets/tablesushi.png'),
-              )
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 30,
+                ),
+                child: Text(
+                  "Heaven of Sushi", 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 20,
+                  left: 30,
+                  right: 30
+                ),
+                child: Text(
+                  "Sushi is a traditional Japanese dish of prepared vinegared rice, usually with some sugar and salt, accompanying a variety of ingredients", 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                  color: Colors.blueGrey[400]
+                  ),
+                ),
+              ),
             ],
           ),
         ),
