fc3196e4895432d61cc42edc75ece11afc669a96
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-10-08 16:34:46 -0600

    adding first route and first screen - home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sushi_app/screens/home.dart';
  
 void main() => runApp(MyApp());
  
@@ -7,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      routes: {
+        '/' : (BuildContext context) => Home()
+      },
     );
   }
 }
